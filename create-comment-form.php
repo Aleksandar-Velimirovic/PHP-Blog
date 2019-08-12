838fb66772be935ac498726f0e3917d9a6c30fb5
Aleksandar Velimirovic <aleksandarvelimirovic2018@gmail.com>
2019-08-12 22:27:01 +0200

    Sidebar now only shows latest posts

--- create-comment-form.php
+++ create-comment-form.php
@@ -1,7 +1,7 @@
 <form method="POST" action="create-comment.php" >
     
     <input id="author" name="author" type="text" placeholder="Author" style="display:block; margin-bottom:1.2rem; padding:0.8rem"/>
-    <textarea id="comment" name="comment" rows="10" cols="100" placeholder="Comment" style="display:block; margin-bottom:1.5rem"></textarea>
+    <textarea id="comment" name="comment" rows="10" cols="100" placeholder="Comment" style="display:block; margin-bottom:1.5rem; max-width: 600px;"></textarea>
     <input type="hidden" value="<?php echo (int)$_GET['post_id'] ?>" name="post_id"/>
     <input class="btn btn-default" type="submit" value="Submit comment">
 
